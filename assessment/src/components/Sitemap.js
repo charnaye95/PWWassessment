e0209685acb898f8e7b6d9cf059ce4ecc2929fe0
Charnaye Grier <charnayegrier@gmail.com>
2022-10-06 17:42:52 -0400

    in the process of working on the sitemap section

--- assessment/src/components/Sitemap.js
+++ assessment/src/components/Sitemap.js
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import dataresearch from '../assets/Data-Research-Icon.svg'
+import geographymapping from '../assets/Geography-Icon.svg'
+import programsservices from '../assets/Programs-Icon.svg'
+import legislativefiscalimpacts from '../assets/Legislative-Icon.svg'
 function Sitemap() {
   return (
-    <div>Sitemap</div>
+    <div className='sitemap-section'>Sitemap
+      <div className='cards'>
+        <div><img src={dataresearch} className='cards-icon' alt=''></img></div>
+        <div><img src={geographymapping} className='cards-icon' alt=''></img></div>
+        <div><img src={programsservices} className='cards-icon' alt=''></img></div>
+        <div><img src={legislativefiscalimpacts} className='cards-icon' alt=''></img></div>
+      </div>
+    </div>
   )
 }
 
