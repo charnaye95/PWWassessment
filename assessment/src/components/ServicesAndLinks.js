00245084ef546a5eb69b6e9f9a79a224f861908f
Charnaye Grier <charnayegrier@gmail.com>
2022-10-07 16:01:27 -0400

    completed services and links section

--- assessment/src/components/ServicesAndLinks.js
+++ assessment/src/components/ServicesAndLinks.js
@@ -2,7 +2,66 @@ import React from 'react'
 
 function ServicesAndLinks() {
   return (
-    <div>ServicesAndLinks</div>
+    <div className='services-section'>
+      <p className='title'>POPULAR SERVICES AND LINKS</p>
+      <div className='block'></div>
+      <div className='services-list'>
+              <a
+                href="https://rfa.sc.gov/data-research/state-finances"
+                className='link'
+              >
+                Overview and History of the South Carolina State Budget
+              </a>
+              <a
+                href="https://rfa.sc.gov/data-research/state-finances"
+                className='link'
+              >
+                Overview and History of the South Carolina State Budget
+              </a>
+              <a
+                href="https://rfa.sc.gov/data-research/state-finances"
+                className='link'
+              >
+                Overview and History of the South Carolina State Budget
+              </a>
+              <a
+                href="https://rfa.sc.gov/sites/default/files/2020-08/Employer%20Contribution%20Charts_Draft_Updated2019.06.03.pdf"
+                className='link'
+              >
+                Employer Contribution Trends
+              </a>
+              <a
+                href="https://rfa.sc.gov/sites/default/files/2020-08/Employer%20Contribution%20Charts_Draft_Updated2019.06.03.pdf"
+                className='link'
+              >
+                Employer Contribution Trends - June 4, 2019
+              </a>
+              <a
+                href="https://rfa.sc.gov/sites/default/files/2020-08/Employer%20Contribution%20Charts_Draft_Updated2019.06.03.pdf"
+                className='link'
+              >
+                Employer Contribution Trends - June 4, 2019
+              </a>
+              <a
+                href="___"
+                className='link'
+              >
+                Appropriation Bills and Acts
+              </a>
+              <a
+                href="___"
+                className='link'
+              >
+                Executive Budget Office <br></br> State Agency Budget Plans(Requests)
+              </a>
+              <a
+                href="___"
+                className='link'
+              >
+                Executive Budget Office <br></br> State Agency Budget Plans(Requests)
+              </a>
+              </div>
+    </div>
   )
 }
 
